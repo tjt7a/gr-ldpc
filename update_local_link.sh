3a28fb176e6c6430fd43f7f7b8a9b30573cd4642
Tom Tracy II <tjt7a@virginia.edu>
2014-06-17 17:52:28 -0400

    Fixed linking issues with remote machines

--- update_local_link.sh
+++ update_local_link.sh
@@ -1,5 +1,5 @@
-cp ldpc_hier_test_link.txt build/apps/CMakeFiles/ldpc_hier_test.dir/link.txt
-cp ldpc_hier_decoding_link.txt build/apps/CMakeFiles/ldpc_hier_decoding.dir/link.txt
-cp ldpc_hier_encoding_link.txt build/apps/CMakeFiles/ldpc_hier_encoding.dir/link.txt
-cp ldpc_child_link.txt build/apps/CMakeFiles/ldpc_child.dir/link.txt
-cp ldpc_parent_link.txt build/apps/CMakeFiles/ldpc_parent.dir/link.txt
+cp local_links/ldpc_hier_test_link.txt build/apps/CMakeFiles/ldpc_hier_test.dir/link.txt
+cp local_links/ldpc_hier_decoding_link.txt build/apps/CMakeFiles/ldpc_hier_decoding.dir/link.txt
+cp local_links/ldpc_hier_encoding_link.txt build/apps/CMakeFiles/ldpc_hier_encoding.dir/link.txt
+cp local_links/ldpc_child_link.txt build/apps/CMakeFiles/ldpc_child.dir/link.txt
+cp local_links/ldpc_parent_link.txt build/apps/CMakeFiles/ldpc_parent.dir/link.txt
---
+++ update_remote_link.sh
@@ -0,0 +1,5 @@
+cp remote_links/ldpc_hier_test_link.txt build/apps/CMakeFiles/ldpc_hier_test.dir/link.txt
+cp remote_links/ldpc_hier_decoding_link.txt build/apps/CMakeFiles/ldpc_hier_decoding.dir/link.txt
+cp remote_links/ldpc_hier_encoding_link.txt build/apps/CMakeFiles/ldpc_hier_encoding.dir/link.txt
+cp remote_links/ldpc_child_link.txt build/apps/CMakeFiles/ldpc_child.dir/link.txt
+cp remote_links/ldpc_parent_link.txt build/apps/CMakeFiles/ldpc_parent.dir/link.txt
